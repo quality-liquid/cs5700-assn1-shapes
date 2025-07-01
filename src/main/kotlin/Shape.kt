98eaf909f4131d6ec57de094c9b060888e8523ca
Benjamin <hickenlooperbe@gmail.com>
2025-06-30 22:18:52 -0600

    wrote the shape class including the abstract getArea function, getpoints function which returns copies, and translate which moves all internal points

--- src/main/kotlin/Shape.kt
+++ src/main/kotlin/Shape.kt
@@ -1,2 +1,19 @@
-class Shape {
+abstract class Shape(val points: List<Point>) {
+
+    abstract fun getArea(): Double
+
+    // returns a list of copies of the actual points
+    fun getPoints(): List<Point> {
+        val copies = mutableListOf<Point>()
+        for (point in this.points) {
+            copies.add(point.clone())
+        }
+        return copies
+    }
+
+    fun translate(deltaX: Double, deltaY: Double) {
+        for (point in this.points) {
+            point.translate(deltaX, deltaY)
+        }
+    }
 }

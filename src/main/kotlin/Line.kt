4f4ff3d2473695d079e967eb290ffb51e8d44d41
Benjamin <hickenlooperbe@gmail.com>
2025-06-30 22:04:34 -0600

    implemented line class including get slope, getLength, and translate methods

--- src/main/kotlin/Line.kt
+++ src/main/kotlin/Line.kt
@@ -1,2 +1,26 @@
-class Line {
+import kotlin.math.sqrt
+
+class Line(val pointA: Point, val pointB: Point) {
+
+    fun getPointA(): Point { return this.pointA.clone() }
+    fun getPointB(): Point { return this.pointB.clone() }
+
+    fun getSlope(): Double {
+        val deltaX = this.pointB.x - this.pointA.x
+        val deltaY = this.pointB.y - this.pointA.y
+        val slope = deltaY / deltaX
+        return slope
+    }
+
+    fun getLength(): Double {
+        val deltaX = this.pointB.x - this.pointA.x
+        val deltaY = this.pointB.y - this.pointA.y
+        val length = sqrt((deltaX * deltaX) + (deltaY * deltaY))
+        return length
+    }
+
+    fun translate(deltaX: Double, deltaY: Double) {
+        this.pointA.translate(deltaX, deltaY)
+        this.pointB.translate(deltaX, deltaY)
+    }
 }

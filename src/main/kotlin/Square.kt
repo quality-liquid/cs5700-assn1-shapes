568444a7fd30f6cc860adb38af281613f98616f8
Benjamin <hickenlooperbe@gmail.com>
2025-07-01 22:00:42 -0600

    implemented my square class

--- src/main/kotlin/Square.kt
+++ src/main/kotlin/Square.kt
@@ -1,6 +1,10 @@
 class Square(points: List<Point>) : Rectangle(points) {
     init {
-        // todo: verify that the difference between the x values is the same as the difference
+        //  verify that the difference between the x values is the same as the difference
         //  between the y values for the two points
+        val (p1, p2) = points
+        if (Math.abs(p1.x - p2.x) != Math.abs(p1.y - p2.y)) {
+            throw IllegalArgumentException("Invalid square points")
+        }
     }
 }
